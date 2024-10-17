b2db41b19f20667de46ac011ca2bf77990c5047d
Yernar Yerboluly <e.erboluly@artox.com>
2024-10-17 19:48:07 +0500

    класс парсер косылды

--- new_parser.php
+++ new_parser.php
@@ -1,3 +1,24 @@
 <?php
 
-echo "жана парсер жасалуда, әлі бітпеді 123 456 789";
+echo "жана парсер жасалуда, әлі бітпеді 123 456 789 салем";
+
+$x  = 123;
+
+function calculate(int $a, int $b)
+{
+    return $a+$b;
+}
+
+class Parser
+{
+    public function __construct()
+    {
+
+    }
+
+    public function parser(string $url)
+    {
+        $html = '<html> <body></body> </html>';
+        return $html;
+    }
+}
